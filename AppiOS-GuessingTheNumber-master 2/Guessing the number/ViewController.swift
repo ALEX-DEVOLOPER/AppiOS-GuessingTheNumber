8e1800ab908d69c9b4ba334bc277f24305e8e0c8
ALEX-DeviOS <75685053+ALEX-DeviOS@users.noreply.github.com>
2020-12-08 16:33:51 +0100

    Add files via upload

---
+++ AppiOS-GuessingTheNumber-master 2/Guessing the number/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guessing the number
+//
+//  Created by Alex on 07/08/2020.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
